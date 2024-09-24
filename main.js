4d5fcbf734ac1ba11a41d46bcf0560cd6582d37e
FlorIniguez <iniguezflorencia788@gmail.com>
2024-09-23 21:56:13 -0300

    arreglando js

--- main.js
+++ main.js
@@ -7,103 +7,96 @@ const icon = document.querySelector(".bx");
 const anchoMitad = window.innerWidth / 2;
 const altoMitad = window.innerHeight / 2;
 let seguirPunteroMouse = true;
+let animacionActiva = false; // Evitar animaciones simultáneas
 
 //------- SEGUIR PUNTERO DEL MOUSE-----------
 body.addEventListener("mousemove", (m) => {
     if (seguirPunteroMouse) {
-      if (m.clientX < anchoMitad && m.clientY < altoMitad) {
-        monster.src = "img/idle/2.png";
-      } else if (m.clientX < anchoMitad && m.clientY > altoMitad) {
-        monster.src = "img/idle/3.png";
-      } else if (m.clientX > anchoMitad && m.clientY < altoMitad) {
-        monster.src = "img/idle/5.png";
-      } else {
-        monster.src = "img/idle/4.png";
-      }
+        if (m.clientX < anchoMitad && m.clientY < altoMitad) {
+            monster.src = "img/idle/2.png";
+        } else if (m.clientX < anchoMitad && m.clientY > altoMitad) {
+            monster.src = "img/idle/3.png";
+        } else if (m.clientX > anchoMitad && m.clientY < altoMitad) {
+            monster.src = "img/idle/5.png";
+        } else {
+            monster.src = "img/idle/4.png";
+        }
     }
-  });
-  
-  
-  // CONTRASEÑA
-  inputClave.addEventListener("focus", () => {
+});
+
+// Función para animar el monstruo cubriendo/descubriendo los ojos
+function cambiarImagenOjos(inicio, fin, velocidad, callback) {
+    if (animacionActiva) return; // Evitar múltiples animaciones
+    animacionActiva = true;
+
+    let count = inicio;
+    const cambiar = () => {
+        monster.src = `img/cover/${count}.png`;
+        if (inicio < fin) {
+            count++;
+            if (count > fin) {
+                animacionActiva = false;
+                if (callback) callback();
+                return;
+            }
+        } else {
+            count--;
+            if (count < fin) {
+                animacionActiva = false;
+                if (callback) callback();
+                return;
+            }
+        }
+        setTimeout(cambiar, velocidad);
+    };
+
+    cambiar();
+}
+
+// CONTRASEÑA
+inputClave.addEventListener("focus", () => {
     seguirPunteroMouse = false;
-    let count = 1;
-    const cubrirOjo = setInterval(() => {
-      monster.src = "img/cover/" + count + ".png";
-      if (count < 8) {
-        count++;
-      } else {
-        clearInterval(cubrirOjo);
-      }
-    }, 60);
-  });
-  inputClave.addEventListener("blur", () => {
+    cambiarImagenOjos(1, 8, 60);
+});
+inputClave.addEventListener("blur", () => {
     seguirPunteroMouse = true;
-    let count = 7;
-    const descubrirOjo = setInterval(() => {
-      monster.src = "img/cover/" + count + ".png";
-      if (count > 1) {
-        count--;
-      } else {
-        clearInterval(descubrirOjo);
-      }
-    }, 60);
-  });
-  
-  
-  
-  // ---- MOSTRAR CONTRASEÑA ----
-  let mostrandoContraseña = false;
-  
-  icon.addEventListener("click", () => {
+    cambiarImagenOjos(7, 1, 60);
+});
+
+// ---- MOSTRAR CONTRASEÑA ----
+let mostrandoContraseña = false;
+
+icon.addEventListener("click", () => {
     if (!mostrandoContraseña) {
-      inputClave.type = "text";
-      icon.classList.remove("bx-show-alt");
-      icon.classList.add("bx-hide");
-    
-  
-      let count = 7;
-      const espiarOjo = setInterval(() => {
-        if (count > 1) {
-          count--;
-          monster.src = "img/cover/" + count + ".png";
-        } else {
-          clearInterval(espiarOjo);
-          mostrandoContraseña = true;
-        }
-      }, 70);
-  
+        inputClave.type = "text";
+        icon.classList.remove("bx-show-alt");
+        icon.classList.add("bx-hide");
+        cambiarImagenOjos(7, 1, 70, () => {
+            mostrandoContraseña = true;
+        });
     } else {
-      inputClave.type = "password";
-      icon.classList.remove("bx-hide");
-      icon.classList.add("bx-show-alt");
-  
-      let count = 1;
-      const taparOjo = setInterval(() => {
-        if (count < 8) {
-          count++;
-          monster.src = "img/cover/" + count + ".png";
-        } else {
-          clearInterval(taparOjo);
-          mostrandoContraseña = false;
-        }
-      }, 70);
+        inputClave.type = "password";
+        icon.classList.remove("bx-hide");
+        icon.classList.add("bx-show-alt");
+        cambiarImagenOjos(1, 8, 70, () => {
+            mostrandoContraseña = false;
+        });
     }
-  });
-  
-  //-----------SEGUIR ESCRITURA----------
-  function cambiarImagen(input) {
+});
+
+//-----------SEGUIR ESCRITURA----------
+function cambiarImagen(input) {
     input.addEventListener('focus', () => {
         seguirPunteroMouse = false;
     });
-  
+
     input.addEventListener('blur', () => {
         seguirPunteroMouse = true;
     });
-  
+
     input.addEventListener('keyup', () => {
         let longitud = input.value.length;
-  
+
         if (longitud >= 0 && longitud <= 5) {
             monster.src = `img/read/1.png`;
         } else if (longitud >= 6 && longitud <= 14) {
@@ -114,17 +107,12 @@ body.addEventListener("mousemove", (m) => {
             monster.src = `img/read/4.png`;
         }
     });
-  }
-  
-  // cambiarImagen(inputEmail);
-  // cambiarImagen(inputUsuario);
-  if (inputEmail) {
+}
+
+if (inputEmail) {
     cambiarImagen(inputEmail);
-  }
-  
-  
-  if (inputUsuario) {
+}
+
+if (inputUsuario) {
     cambiarImagen(inputUsuario);
-  }
-  
-  
+}
